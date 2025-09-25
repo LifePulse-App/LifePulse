831274a58f0f8b712dacb14e5ef360c5ed18c7fd
Syed Ali Asghar <aligee512@gmail.com>
2025-09-25 13:41:01 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Test route
-app.get('/', (req, res) => {
-  res.send('Backend API is running 🚀');
-});
-
 // Pick port based on environment
 const ENV = process.env.NODE_ENV;
 const PORT = ENV === 'development' ? 4000 : 3000;  // lowercase 'development', no extra space
+
+// Test route
+app.get('/health', (req, res) => {
+  res.send(`Backend API is running on ${PORT} in ${ENV} mode🚀`);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT} in ${ENV} mode`);
