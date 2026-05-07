e07bfc99ac97412f8036ad6ee4a22da31af96c53
Syed Ali Asghar <aligee512@gmail.com>
2026-05-07 17:10:36 +0500

    added seo branding

--- frontend/src/auth/api-client/api_client.tsx
+++ frontend/src/auth/api-client/api_client.tsx
@@ -16,7 +16,7 @@ const processQueue = (error: any, token: string | null = null) => {
 };
 
 const apiClient = create({
-  baseURL: 'http://10.90.16.124:40000/api',
+  baseURL: 'https://api-prod.streaksphere.app/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
