2c51555460e95fc23782f0fc04c460562c6c49f6
Syed Ali Asghar <aligee512@gmail.com>
2026-07-03 15:24:57 +0500

    test

--- backend/middlewares/api-middleware.js
+++ backend/middlewares/api-middleware.js
@@ -3,7 +3,14 @@ import ErrorHandler from "../utils/errorHandler.js";
 export default function apiKeyMiddleware(req, res, next) {
   const apiKey = req.headers["api-key"]; // Expect API key in headers
 
-  if (!apiKey || apiKey !== process.env.API_KEY) {
+  console.log(apiKey);
+
+  if (!apiKey) {
+    return next(new ErrorHandler("Forbidden: Missing Api Key", 403));
+  }
+  
+
+  if (apiKey !== process.env.API_KEY) {
     return next(new ErrorHandler("Forbidden: Invalid API key", 403));
   }
 
