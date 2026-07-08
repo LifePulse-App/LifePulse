4fb0a401e12a96456f9fb1315bf9be768db76e7e
Syed Ali Asghar <aligee512@gmail.com>
2026-07-08 21:00:39 +0500

    v5.0.0 release

--- frontend/ios/StreakSphere/AppDelegate.swift
+++ frontend/ios/StreakSphere/AppDelegate.swift
@@ -3,6 +3,7 @@ import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
 import FirebaseCore
+import CodePush
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
-        // 🔥 MUST be first — before anything else
+    // 🔥 MUST be first — before anything else
     FirebaseApp.configure()
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
@@ -43,9 +44,9 @@ class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
 
   override func bundleURL() -> URL? {
 #if DEBUG
-    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+    return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
 #else
-    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+    return CodePush.bundleURL()
 #endif
   }
 }
